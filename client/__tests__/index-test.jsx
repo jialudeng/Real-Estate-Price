e8bc16d88fb0d57cb0c319991ed2bb5f12617204
Jialu Deng <jialudeng1009@gmail.com>
2020-02-04 14:14:09 -0800

    attempted fixing and failed

--- client/__tests__/index-test.jsx
+++ client/__tests__/index-test.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Graph from '../src/components/index';
+import Title from '../src/components/Title';
+import Subtitle from '../src/components/Subtitle';
 
 describe('<Graph />', () => {
   it('renders children when passed in', () => {
     const wrapper = shallow((
-      <Graph>
-        <div className="unique" />
-      </Graph>
+      <Graph />
     ));
-    expect(wrapper.contains(<div className="unique" />)).toBe(true);
+    expect(wrapper.contains([<Title />, <Subtitle />]));
   });
 });
